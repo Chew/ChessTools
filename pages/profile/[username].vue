adecb191d2bef836b8bca14e79439862693eabbe
Chew <chew@chew.pw>
2023-09-26 10:07:01 -0500

    more error handling on profile page

--- pages/profile/[username].vue
+++ pages/profile/[username].vue
@@ -1,5 +1,8 @@
 <template>
-  <main>
+  <main v-if="error">
+    {{ message }}
+  </main>
+  <main v-else-if="user">
     <h1>Profile for {{ user.username }}</h1>
     <p>{{ user.bio }}</p>
   </main>
@@ -16,10 +19,29 @@ export default defineComponent({
   setup() {
     const username = useRoute().params.username;
 
-    const {data: userData} = useFetch("/api/users/" + username);
+    const {data: userData, error: userError} = useFetch<{ success: boolean }>("/api/users/" + username);
+    const userDataValue = userData.value;
+
+    if (userDataValue == null) {
+      return {
+        error: true,
+        message: "Error loading user data: " + userError?.value || "Unknown error",
+        user: null,
+      }
+    }
+
+    if (userDataValue.success == false) {
+      return {
+        error: true,
+        message: "User not found",
+        user: null,
+      }
+    }
 
     // set data for the page
     return {
+      error: false,
+      message: "",
       user: (userData.value as { success: boolean, data: TableUser }).data
     }
   },
