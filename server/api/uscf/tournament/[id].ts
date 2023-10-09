a9c902a15618a544268934cff40f253a2b0172f7
Chew <chew@chew.pw>
2023-10-08 21:03:21 -0500

    support for uscf tournament pages

---
+++ server/api/uscf/tournament/[id].ts
@@ -0,0 +1,163 @@
+import { parse } from 'node-html-parser'
+import { gatherElements, findElement, nameAndIdFormatter } from '~/utils/elements'
+import {
+    USCFTournamentRatingType,
+    USCFTournamentSection,
+    USCFTournamentSectionPlayer
+} from '~/types/uscf'
+
+export default defineEventHandler(async (event) => {
+    const id = parseInt(getRouterParam(event, 'id') || '0')
+
+    // Fetch the HTML page from the USCF website and parse it
+    const data = await fetch('https://www.uschess.org/msa/XtblMain.php?' + id + '.0')
+        .then(response => response.text())
+        .then((data) => {
+            return data
+                // remove <nobr> tags
+                .replaceAll('<nobr>', '').replaceAll('</nobr>', '')
+                // fix one of 103 errors on the page
+                .replaceAll('<tr class=topbar></td>', '<tr class=topbar>')
+        })
+        .then((data) => {
+            return parse(data)
+        })
+
+    // This is specifically the section tables.
+    // This will be 2x the amount of sections, odd numbered tables have everything, even numbered tables are just the data formatted better
+    const tables: string[][][] = []
+    data.querySelectorAll('table').forEach((table) => {
+        const elements: string[][] = gatherElements(table)
+
+        tables.push(elements)
+    })
+
+    const elements = gatherElements(data)
+
+    const processedInfo = findElement(elements, 'Processed')
+    let processed = null
+    if (processedInfo) {
+        processed = {
+            received: processedInfo.split('  ')[0].replace('Received: ', ''),
+            entered: processedInfo.split('  ')[1].replace('Entered: ', ''),
+            rated: processedInfo.split('  ')[2].replace('Rated: ', '')
+        }
+    }
+
+    const sections: USCFTournamentSection[] = []
+    // SECTION TIME :) :) :) :) :) :) :) :)
+    for (let i = 0; i < tables.length; i += 2) {
+        const section = tables[i]
+        const sectionInfo = tables[i + 1]
+
+        const sectionID = sectionInfo[0][0]
+
+        const processedData = sectionInfo[2][1]
+
+        const processedInfo = {
+            received: processedData.split('Received: ')[1].split(' ')[0],
+            entered: processedData.split('Entered: ')[1].split(' ')[0],
+            rated: processedData.split('Rated: ')[1].split(' ')[0],
+            reRated: undefined as string | undefined
+        }
+
+        if (processedData.includes('Re-Rated')) {
+            processedInfo.reRated = processedData.split('Re-Rated: ')[1].split(' ')[0]
+        }
+
+        const statsData = sectionInfo[3][1]
+        const statsInfo = {
+            rounds: parseInt(statsData.split(' ')[0]),
+            players: parseInt(statsData.split(' Rounds, ')[1].split(' Players')[0]),
+            kFactor: statsData.split('  ')[1].split(' ')[2],
+            ratingSystem: statsData.split('  ')[2].split(' ')[2],
+            tournamentType: statsData.split('  ')[3].trim().split(' ')[2],
+            timeControl: statsData.split('  ')[4].split(' ')[2]
+        }
+
+        const players: USCFTournamentSectionPlayer[] = []
+        const playerInfo = section[5][0].split('\n')
+        const base = 13
+        let rowsOfData = 0
+        while (!playerInfo[base + (rowsOfData)].includes('---------')) {
+            rowsOfData++
+        }
+        for (let i = 0; i < statsInfo.players; i++) {
+            const start = base + (i * (rowsOfData + 1))
+            const info = []
+            for (let j = 0; j < rowsOfData; j++) {
+                info.push(playerInfo[start + j].split('|').map(ele => ele.trim()))
+            }
+
+            const rounds = []
+            for (let j = 0; j < statsInfo.rounds; j++) {
+                const round = {
+                    roundNumber: j + 1,
+                    result: info[0][3 + j].split(' ')[0],
+                    color: info[1][3 + j],
+                    opponentPairNumber: parseInt(info[0][3 + j].split('   ')[1])
+                }
+
+                rounds.push(round)
+            }
+
+            const totalRatingChanges = info.length - 1
+
+            const ratings: Record<string, object> = {}
+            for (let j = 0; j < totalRatingChanges; j++) {
+                let rating = info[j + 1][1]
+                if (j === 0) {
+                    rating = rating.split('/')[1]
+                }
+
+                const type: keyof typeof USCFTournamentRatingType = rating.split(':')[0].trim() as keyof typeof USCFTournamentRatingType
+                const pre = parseInt(rating.split(':')[1].split('->')[0])
+                const post = parseInt(rating.split(':')[1].split('->')[1])
+
+                ratings[USCFTournamentRatingType[type]] = {
+                    pre,
+                    post
+                }
+            }
+
+            const player: USCFTournamentSectionPlayer = {
+                pairNumber: parseInt(parse(info[0][0]).text),
+                name: parse(info[0][1]).text,
+                totalPoints: parseFloat(info[0][2]),
+                state: info[1][0],
+                memberId: parseInt(info[1][1].split('/')[0]),
+                rounds,
+                ratings
+            }
+
+            players.push(player)
+        }
+
+        const sectionData: USCFTournamentSection = {
+            id: parseInt(sectionID.split(' - ')[0].split('Section ')[1]),
+            name: sectionID.split(' - ')[1],
+            processed: processedInfo,
+            stats: statsInfo,
+            players
+        }
+
+        sections.push(sectionData)
+    }
+
+    // Return everything :)
+    return {
+        success: true,
+        error: null,
+
+        summary: {
+            event: nameAndIdFormatter(findElement(elements, 'Event') || ''),
+            location: findElement(elements, 'Location'),
+            eventDates: findElement(elements, 'Event Date(s)'),
+            affiliate: nameAndIdFormatter(findElement(elements, 'Sponsoring Affiliate') || ''),
+            chiefTd: nameAndIdFormatter(findElement(elements, 'Chief TD') || ''),
+            processed
+        },
+
+        sections
+    }
+})
--- types/uscf.ts
+++ types/uscf.ts
@@ -80,3 +80,105 @@ export interface USCFMember {
     },
     lastChange: string,
 }
+
+export interface USCFTournamentSectionPlayer {
+    pairNumber: number;
+    state: string;
+    name: string;
+    memberId: number;
+    totalPoints: number;
+    ratings: {
+        regular?: {
+            pre: number;
+            post: number;
+        } | null
+        quick?: {
+            pre: number;
+            post: number;
+        } | null
+        blitz?: {
+            pre: number;
+            post: number;
+        } | null
+        onlineRegular?: {
+            pre: number;
+            post: number;
+        } | null
+        onlineQuick?: {
+            pre: number;
+            post: number;
+        } | null
+        onlineBlitz?: {
+            pre: number;
+            post: number;
+        } | null
+    }
+    rounds: {
+        roundNumber: number;
+        result: string;
+        color: string;
+        opponentPairNumber: number;
+    }[]
+}
+
+export interface USCFTournamentSection {
+    id: number;
+    name: string;
+    processed: {
+        received: string;
+        entered: string;
+        rated: string;
+        reRated?: string
+    };
+    stats: {
+        rounds: number;
+        players: number;
+        kFactor: string;
+        ratingSystem: string;
+        tournamentType: string;
+        timeControl: string;
+    },
+    players: USCFTournamentSectionPlayer[]
+}
+
+export interface USCFTournament {
+    success: boolean;
+    error: string | null;
+    summary: {
+        event: {
+            name: string;
+            id: number;
+        };
+        location: string;
+        eventDates: string;
+        affiliate: {
+            name: string;
+            id: number;
+        }
+        chiefTd: {
+            name: string;
+            id: number;
+        }
+        processed: {
+            received: string;
+            entered: string;
+            rated: string;
+        }
+    }
+    sections: USCFTournamentSection[];
+}
+
+export enum USCFTournamentRatingType {
+    // eslint-disable-next-line no-unused-vars
+    R = 'regular',
+    // eslint-disable-next-line no-unused-vars
+    Q = 'quick',
+    // eslint-disable-next-line no-unused-vars
+    B = 'blitz',
+    // eslint-disable-next-line no-unused-vars
+    OR = 'onlineRegular',
+    // eslint-disable-next-line no-unused-vars
+    OQ = 'onlineQuick',
+    // eslint-disable-next-line no-unused-vars
+    OB = 'onlineBlitz',
+}
